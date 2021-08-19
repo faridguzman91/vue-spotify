77501efa3575a57f626d239f2c595e065c5d52b5
Jimmy Guzman <jimmy@creapolis.com>
2021-08-19 14:45:42 +0200

    init spotify playlist

---
+++ src/components/spotifyIndex.vue
@@ -0,0 +1,34 @@
+<template>
+  <table class="table is-fullwidth is-striped is-hoverable is-narrow">
+    <thead>
+      <th>Artist</th>
+      <th>Title</th>
+      <th>Album</th>
+      <th>Duration</th>
+    </thead>
+
+    <tbody v-for="(song, index) in songs" v-bind:key="index">
+      <tr>
+      <td> {{ song.artist }}</td>
+      <td> {{ song.title }}</td>
+      <td> {{ song.album }}</td>
+      <td> {{ song.duration }} </td>
+
+      </tr>
+
+    </tbody>
+  </table>
+</template>
+
+<script>
+import playlistData from "@/assets/spotifyplaylist.json";
+export default {
+  data() {
+    return {
+      songs: playlistData,
+    };
+  },
+};
+</script>
+
+<style></style>
